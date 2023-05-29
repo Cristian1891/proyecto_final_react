a0251ce2f8ecea75737cd2619307aefaca4a98bb
Cristian1891 <83612315+Cristian1891@users.noreply.github.com>
2023-05-29 01:31:31 -0300

    Cambio de nombre de lista

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,7 +4,7 @@ import './ItemCard.css'
 
 export const ItemCard = ({item}) => {
   return (
-    <>
+    <> 
 
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
