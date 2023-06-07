2775251ef9f88b381581d84353a2bd50759fc50c
Cristian1891 <83612315+Cristian1891@users.noreply.github.com>
2023-06-07 05:21:01 -0300

    prueba HashRouter 9

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -9,7 +9,6 @@ import { AuthContext } from "../context/AuthContext";
 import { Checkout } from "../components/Checkout/Checkout";
 import { NavBar } from "../components/NavBar/NavBar"
 import { Footer } from "../components/Footer/Footer"
-import { Error404 } from "../components/Error404/Error404";
 
 
 export const AppRouter = () => {
@@ -30,7 +29,7 @@ export const AppRouter = () => {
                     <Route path='/' element={ <ItemListContainer/> }/>
                     <Route path='/category/:categoryId' element={ <ItemListContainer />}/>
                     <Route path='/item/:itemId' element={ <ItemDetailContainer/> }/>
-                    <Route path='*' element={<Error404/>} />
+                    <Route path='*' element={ <Navigate to={"/"}/>} />
                     <Route path='/cart' element={<Cart/>}/>
                     <Route path='/checkout' element={ <Checkout/> }/>
                 </Routes>
@@ -40,7 +39,7 @@ export const AppRouter = () => {
                     {/* rutas públicas */}
                     <Route path='/login' element={<LoginScreen/>}/>
                     <Route path='/register' element={<RegisterScreen/>}/>
-                    <Route path='*' element={<Error404/>}/>
+                    <Route path='*' element={ <Navigate to="/login"/>}/>
                 </Routes>
         }
         
